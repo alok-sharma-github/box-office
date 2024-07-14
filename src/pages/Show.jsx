4b8643f0182a9e0e7b28f66bd21cc1069a6ffff4
Alok Sharma <kingofkingsalok@gmail.com>
2024-07-15 02:38:05 +0530

    user-effect-hook-changed-with-REACT-QUERY. show data

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/tvmaze";
+import { useQuery } from "@tanstack/react-query";
+
+// const useShowById = (showId) => {
+//   const [showData, setShowData] = useState(null);
+//   const [showError, setShowError] = useState(null);
+//   useEffect(() => {
+//     async function fetchData() {
+//       try {
+//         const data = await getShowById(showId);
+//         setShowData(data);
+//       } catch (err) {
+//         setShowError(err.message);
+//       }
+//     }
+//     fetchData();
+//   }, [showId]);
+//   return [showData, showError];
+// };
 
 const Show = () => {
   const { showId } = useParams();
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(err.message);
-      }
-    }
-    fetchData();
-  }, [showId]);
+  //   const [showData, showError] = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ["show", "showId"],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError}</div>;
@@ -27,7 +36,7 @@ const Show = () => {
     return (
       <div>
         <h1>{showData.name}</h1>
-        <p>{showData.summary}</p>
+        <p dangerouslySetInnerHTML={{ __html: showData.summary }} />
         {/* Add more show details here */}
       </div>
     );
