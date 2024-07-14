86100a3ac33e98fc3f83e4b2e6acb3e341d6bd6f
Alok Sharma <kingofkingsalok@gmail.com>
2024-07-14 13:39:02 +0530

    reusable fun to fetch and display data.

---
+++ src/api/tvmaze.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://api.tvmaze.com";
+
+const apiGet = async (queryString) => {
+  try {
+    const response = await fetch(`${BASE_URL}${queryString}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const body = await response.json();
+    console.log(body);
+    return body;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    throw error; // Re-throw the error so it can be handled by the caller
+  }
+};
+
+export const searchForShows = (query) => apiGet(`/search/shows?q=${query}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { searchForShows } from "./../api/tvmaze";
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
   const onSearchInputChange = (e) => {
     setSearchStr(e.target.value);
@@ -9,11 +12,29 @@ const Home = () => {
 
   const onSearch = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q=${searchStr}`
-    );
-    const body = await response.json();
-    console.log(body);
+    setApiDataError(null); // Reset error state before new search
+    setApiData(null); // Reset apiData state before new search
+    try {
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (err) {
+      setApiDataError(err.message);
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error: {apiDataError}</div>;
+    }
+    if (apiData) {
+      if (apiData.length === 0) {
+        return <div>Not Found</div>;
+      }
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
   };
 
   return (
@@ -22,6 +43,7 @@ const Home = () => {
         <input type="text" value={searchStr} onChange={onSearchInputChange} />
         <button type="submit">Search</button>
       </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 };
